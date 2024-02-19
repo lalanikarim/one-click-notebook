d99ade1bb636029299976a8db71a7e6a37efd959
Karim Lalani <jimmy00784@gmail.com>
2024-02-18 23:55:41 -0600

    adding client js

---
+++ notebook.js
@@ -0,0 +1,16 @@
+function create_buttons() {
+	var colab = document.createElement("a")
+	colab.href = 'https://one-click-notebook.one-click-notebook.workers.dev/?on=colab&url=' + window.location
+	var colab_img = document.createElement("img")
+	colab_img.src = "https://colab.research.google.com/assets/colab-badge.svg"
+	colab_img.alt = "Open in Colab"
+	colab.append(colab_img)
+	element.append(colab)
+	var paperspace = document.createElement("a")
+	paperspace.href = 'https://one-click-notebook.one-click-notebook.workers.dev/?on=paperspace&url=' + window.location
+	var paperspace_img = document.createElement("img")
+	paperspace_img.src = "https://assets.paperspace.io/img/gradient-badge.svg"
+	paperspace_img.alt = "Run on Gradient"
+	paperspace.append(paperspace_img)
+	element.append(paperspace)
+}
